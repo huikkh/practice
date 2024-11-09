234f2117942f64bbe92fd54993d153e140599fef
huikkh <144972281+huikkh@users.noreply.github.com>
2024-11-09 11:19:59 +0800

    Add files via upload

---
+++ 判斷基數偶數.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    int num;
+    scanf("%d", &num);
+
+    if(num % 2 == 0){
+        printf("even\n");
+    }
+    else{
+        printf("odd\n");
+    }
+
+
+
+
+	return 0;
+}
